5dc4f95052754d7fb8e93fea2c2337bd31b56561
mohkap111101 <mohkap11@outlook.com>
2024-07-25 18:59:48 +0100

    style login page using v0

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,8 @@
+import { LoginPage } from '@/components/login-page';
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  return <LoginPage />;
 }
 
 export default Login
